191b94a88d86ad9d33aa0a0773393636bada40e8
bharatpanwar4 <bharatpanwar4@gmail.com>
2024-07-18 00:22:13 +0530

    created theme changer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,12 @@
 'use client'
-import { Button } from "@/src/lib/ui/button";
-import { useTheme } from "next-themes";
-import Image from "next/image";
+
+import ThemeController from "@/src/common/ThemeController";
 
 export default function Home() {
-  const { setTheme } = useTheme()
+ 
   return (
    <div className="flex items-center justify-around pt-8">
-
-    <Button onClick={() => setTheme("light")}>
-          Light
-        </Button>
-        <Button onClick={() => setTheme("dark")}>
-          Dark
-        </Button>
-        <Button onClick={() => setTheme("system")}>
-          System
-        </Button>
+<ThemeController/>
    </div>
   );
 }
---
+++ src/common/ThemeController.tsx
@@ -0,0 +1,39 @@
+import { useTheme } from 'next-themes'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/src/lib/ui/dropdown"
+import { Button } from '../lib/ui/button'
+import { Moon, Sun } from 'lucide-react'
+  
+const ThemeController = () => {
+    const { setTheme } = useTheme()
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+export default ThemeController
